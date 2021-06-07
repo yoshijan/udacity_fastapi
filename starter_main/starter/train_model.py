db1ba9997cf94b14710f6332ee96f32d686410e8
yoshijan <jan.geukes@gmail.com>
2021-06-07 21:58:16 +0200

    add slice_output.txt and function to create

--- starter_main/starter/train_model.py
+++ starter_main/starter/train_model.py
@@ -5,6 +5,9 @@ import pandas as pd
 # Add the necessary imports for the starter code.
 import starter_main.starter.ml.data as mldata
 import starter_main.starter.ml.model as mlmodel
+
+#import ml.data as mldata
+#import ml.model as mlmodel
 from sklearn.model_selection import train_test_split
 
 
@@ -66,6 +69,67 @@ def train_model():
 
     print(loaded_model.coef_.shape)
 
+def fixed_variable_metric(variable_name, testdata, logfile):
+    #load models
+    print("load model")
+    loaded_model = mlmodel.load_model('model.pkl', basepath="model")
+    print("load encoder")
+    encoder = mlmodel.load_model('encoder.pkl', basepath='model')
+    print("load lb")
+    lb = mlmodel.load_model('lb.pkl', basepath='model')
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    #get for each categorical value
+    for value in testdata[variable_name].unique():
+        logfile.write("calculate metrics for " + value + "\n")
+        #fix value
+        test_set = testdata[testdata[variable_name] == value]
+        X_test, y_test, encoder, lb = mldata.process_data(
+            test_set, categorical_features=cat_features, label="salary", training=False,
+            encoder=encoder, lb=lb
+        )
+        preds_test = mlmodel.inference(loaded_model,X_test)
+        #calc score
+        precision, recall, fbeta =mlmodel.compute_model_metrics(y_test,preds_test)
+        logfile.write("precision: " + str(precision) + ", recall: " + str(recall) + ", fbeta: " + str(fbeta) +  "\n")
+        logfile.write("----------" +  "\n")
+
+
+    #set categorial variable to fixed value
+    #calculate metric
+
+def run_fixed_variable_metric():
+    with open("slice_output.txt", "a") as logfile:
+        #run slices for all categorical features
+        logfile.write("Start run \n")
+        data = pd.read_csv('data/census_clean.csv')
+        train, test = train_test_split(data, test_size=0.20)
+        cat_features = [
+            "workclass",
+            "education",
+            "marital-status",
+            "occupation",
+            "relationship",
+            "race",
+            "sex",
+            "native-country",
+        ]
+
+        for variable_name in cat_features:
+            logfile.write("### Evaluation for " + variable_name + " ### \n")
+            fixed_variable_metric(variable_name, test, logfile)
+
+
+
+
 #seperate function for model performacne output on a set of categorical features
 def silice_test_performance(cat_features):
     print("read pandas")
@@ -99,4 +163,6 @@ def silice_test_performance(cat_features):
 
 
 if __name__ == "__main__":
-    train_model()
+    #train_model()
+    run_fixed_variable_metric()
+
